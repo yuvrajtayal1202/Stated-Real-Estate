546b7e252ebd9c950083df0ac903caf349135f34
Yuvraj Tayal <yuvrajtayal1202@gmail.com>
2025-03-29 12:19:15 +0530

    Initial commit

---
+++ JS/testimonial.js
@@ -0,0 +1,47 @@
+$(function () {
+    let headerElem = $('header');
+    let logo = $('#logo');
+    let navMenu = $('#nav-menu');
+    let navToggle = $('#nav-toggle');
+
+   $('#properties-slider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        prevArrow: '<a href="#" class="slick-arrow slick-prev">previous</a>',
+        nextArrow: '<a href="#" class="slick-arrow slick-next">next</a>',
+        responsive: [
+            {
+                breakpoint: 1100,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    infinite: true,
+                }
+            },
+            {
+                breakpoint: 767,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    infinite: true,
+                }
+            },
+            {
+                breakpoint: 530,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    infinite: true,
+                }
+            }
+        ]
+   });
+
+   $('#testimonials-slider').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        prevArrow: '<a href="#" class="slick-arrow slick-prev"><</a>',
+        nextArrow: '<a href="#" class="slick-arrow slick-next">></a>'
+   });
+})
